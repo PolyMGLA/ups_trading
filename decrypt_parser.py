2c723cf4c8b8ee42079062f7c639d88dc0beddbc
MrKreeper1 <timofey.blinov@icloud.com>
2025-01-11 17:07:17 +0300

    fix decrypt_parser.py

--- decrypt_parser.py
+++ decrypt_parser.py
@@ -3,16 +3,44 @@ from bs4 import BeautifulSoup
 import json
 import os
 import os.path
+import tqdm
+import time
 
+from joblib import Parallel, delayed
+
+import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 FILENAME_DEF = "decrypt_news.json"
+NUM_DEF = 4
+N_CORES = 4
 
-def DecryptParser(FILENAME):
+def parse_url(h):
+    global parsed
+    if h in parsed:
+        # print("skipping:", h)
+        return { }
+    # print(h)
+    time.sleep(.25)
+    req = requests.get(h)
+    soup = BeautifulSoup(req.text, "lxml")
+    # print(soup.find_all("h1"))
+    section = soup.find("div", { "class": "z-2 flex-1 min-w-0" })
+    caption = section.find("h1", { "class": "font-canela font-black text-neutral-800" }).getText()
+    date = soup.find("span", { "class": "font-akzidenz-grotesk scene:font-itc-avant-garde-gothic-pro scene:font-light font-normal text-sm/4.5 md:text-base/5 xl:text-lg/5 scene:text-sm whitespace-nowrap" }).find("time").getText()
+    #date = date.replace("Updated", "")[1:13].replace(",", "")
+    text = ""
+    for el in section.find_all("p"):
+        text += el.getText() + "\n"
+    parsed[h] = [caption, date, text]
+    return { h: [caption, date, text] }
+
+
+def DecryptParser(FILENAME, NUM):
     """
-    Парсит новости с Decrypt.co
+    Парсит новости с Decrypt.so
     Результат сохраняет в FILENAME
     Формат: json
     {
@@ -20,20 +48,52 @@ def DecryptParser(FILENAME):
     }
     Если ссылка уже присутствует в json, парсинг прекращается
     """
-    DECRYPT_ADDR = "https://decrypt.co"
-    with open(FILENAME, "r") as f: parsed = json.load(f)
+    COINDESK_ADDR = "https://decrypt.co"
+    global parsed
+    with open(FILENAME, "r") as f: p = json.load(f)
+    parsed = { }
+    for el in p:
+        if el == { }: continue
+        parsed[el] = p[el]
+    # print(parsed)
 
-    r = requests.get(DECRYPT_ADDR + "/news")
-    soup = BeautifulSoup(r.text, "lxml")
-
-    hrefs = []
-    for el in soup.find_all("div", { "class": "mb-5 pb-5 last-of-type:mb-0" }):
-        hrefs.append(DECRYPT_ADDR + el.find("a", { "class": "linkbox__overlay" },href=True)["href"])
-        print(DECRYPT_ADDR + el.find("a", { "class": "linkbox__overlay" },href=True)["href"])
+    #r = requests.get(COINDESK_ADDR + "/latest-crypto-news")
+    driver = webdriver.Firefox()
+    driver.set_page_load_timeout(5)
+    try:
+        driver.get(COINDESK_ADDR + "/news")
+    except selenium.common.exceptions.TimeoutException as e:
+        pass
+    btn = None
+    for el in driver.find_elements(By.TAG_NAME, "button")[::-1]:
+        if "Load More" in el.get_attribute("outerHTML"):
+            btn = el
+            break
+    # print("press q to break")
+    for i in tqdm.tqdm(range(NUM)):
+        driver.execute_script("arguments[0].click();", btn)
+        time.sleep(.65)
     
-    with open(FILENAME, "w") as f: json.dump(parsed, f)
+    # print("interrupted on i =", i)
+    
+    soup = BeautifulSoup(driver.page_source, "lxml")
+    driver.close()
+    hrefs = []
+    for el in soup.find_all("div", { "class": "flex flex-col border-l-[0.5px] ml-0.5 border-neutral-300 pl-2 md:pl-3 xl:pl-4 pt-7" }):
+        # print(el)
+        # print(el.find("a", { "class": "linkbox__overlay" }, href=True))
+        hrefs.append(COINDESK_ADDR + el.find("a", { "class": "linkbox__overlay" }, href=True)["href"])
+    print("parsing..")
+    
+    data = Parallel(n_jobs=N_CORES, verbose=10)(delayed(parse_url)(h) for h in tqdm.tqdm(hrefs))
+    # print(data)
+    for el in data:
+        if el == { }: continue
+        # print(el)
+        parsed[list(el.keys())[0]] = list(el.values())[0]
+    with open(FILENAME, "w") as f: json.dump(parsed, f, indent=4)
 
 if __name__ == "__main__":
     if not os.path.exists(FILENAME_DEF):
         with open(FILENAME_DEF, "w") as f: f.write("{}")
-    DecryptParser(FILENAME=FILENAME_DEF)
+    DecryptParser(FILENAME=FILENAME_DEF, NUM=NUM_DEF)
