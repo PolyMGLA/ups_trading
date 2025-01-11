7b5fa882af00e1b228b3b9bebabf93718d806cfd
unknown <mr-Lime197>
2025-01-11 09:40:49 +0300

    new func 'Get_metrics'

--- fin_metrics.py
+++ fin_metrics.py
@@ -1,11 +1,12 @@
 import numpy as np
 
 def PnL(x):
-    return x.sum(axis=1).sum()
+    '''x here it is alpha*returns'''
+    return x.sum().sum()
 def Sharpe(x):
     return PnL(x) / x.sum(axis=1).std()
 def Drawdown(x):
-    pnl_vector = x.sum()
+    pnl_vector = x.sum(axis=1)
     return max((pnl_vector.cummax() - pnl_vector.cumsum()) / pnl_vector.cummax())
 def Turnover(alpha):
     turnovers = []
@@ -17,3 +18,5 @@ def Turnovers_distribution(alpha):
     for i in range(1, len(alpha)):
         turnovers.append((alpha.iloc[i] - alpha.iloc[i-1]).abs().sum())
     return np.array(turnovers)
+def Get_metrics(x,alpha):
+    return {"Pnl": PnL(x), "Sharpe": Sharpe(x), "Drawdown": Drawdown(x), "Turnover": Turnover(alpha)}
