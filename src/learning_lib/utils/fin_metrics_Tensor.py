87e94222e15b462e035e02045a6b920a72f40899
unknown <mr-Lime197>
2025-01-17 09:41:00 +0300

    Fix: remake func for tensors

--- src/learning_lib/utils/fin_metrics_Tensor.py
+++ src/learning_lib/utils/fin_metrics_Tensor.py
@@ -10,61 +10,61 @@ class FinCalculationsTensor:
 
     @staticmethod
     def pnl_tensor(
-        alpha: pt.Torch,
-        returns: pt.Torch    
-    ) -> pt.Torch:
+        alpha: pt.tensor,
+        returns: pt.tensor    
+    ) -> pt.tensor:
         '''
         Метод для расчета вектора доходности (PnL) по альфе и доходности активов.
         Альфа умножается на доходность для получения доходности по каждому активу.
         
-        :param alpha: Датафрейм с весами альфа-стратегии.
-        :param returns: Датафрейм с доходностью активов.
-        :return: Серия с результатами доходности по каждому временному интервалу.
+        :param alpha: tensor с весами альфа-стратегии.
+        :param returns: tensor с доходностью активов.
+        :return: tensor с результатами доходности по каждому временному интервалу.
         '''
         return pt.multiply(alpha, returns).sum(dim=1)
     
     @staticmethod
     def pnl(
-        alpha: pd.DataFrame,
-        returns: pd.DataFrame
-    ) -> float:
+        alpha: pt.tensor,
+        returns: pt.tensor
+    ) -> pt.tensor:
         '''
         Метод для расчета общего значения доходности (PnL).
         Суммируется весь результат из вектора доходности (PnL).
         
-        :param alpha: Датафрейм с весами альфа-стратегии.
-        :param returns: Датафрейм с доходностью активов.
+        :param alpha: tensor с весами альфа-стратегии.
+        :param returns: tensor с доходностью активов.
         :return: Финальное значение PnL.
         '''
         return FinCalculationsTensor.pnl_tensor(alpha, returns).sum(dim=0)
     
     @staticmethod
     def sharpe(
-        alpha: pd.DataFrame,
-        returns: pd.DataFrame
-    ) -> pd.Series:
+        alpha: pt.tensor,
+        returns: pt.tensor
+    ) -> pt.tensor:
         '''
         Метод для расчета коэффициента Шарпа.
         Он вычисляется как отношение средней доходности (PnL) к стандартному отклонению доходности (PnL).
         
-        :param alpha: Датафрейм с весами альфа-стратегии.
-        :param returns: Датафрейм с доходностью активов.
+        :param alpha: tensor с весами альфа-стратегии.
+        :param returns: tensor с доходностью активов.
         :return: Коэффициент Шарпа для данной альфа-стратегии.
         '''
         return pt.multiply(alpha, returns).sum() / pt.multiply(alpha, returns).std()
     
     @staticmethod
     def drawdown_tensor(
-        alpha: pd.DataFrame,
-        returns: pd.DataFrame
-    ) -> pd.Series:
+        alpha: pt.tensor,
+        returns: pt.tensor
+    ) -> pt.tensor:
         '''
         Метод для расчета вектора просадки (drawdown).
         Просадка вычисляется как разница между текущим пиком накопленной доходности и текущей доходностью.
         
-        :param alpha: Датафрейм с весами альфа-стратегии.
-        :param returns: Датафрейм с доходностью активов.
-        :return: Вектор просадок для каждого временного интервала.
+        :param alpha: tensor с весами альфа-стратегии.
+        :param returns: tensor с доходностью активов.
+        :return: tensor просадок для каждого временного интервала.
         '''
         return (
             FinCalculationsTensor.pnl_tensor(alpha, returns).cummax() - \
@@ -75,69 +75,70 @@ class FinCalculationsTensor:
     
     @staticmethod
     def maxDrawdown(
-        alpha: pd.DataFrame,
-        returns: pd.DataFrame
-    ) -> float:
+        alpha: pt.tensor,
+        returns: pt.tensor
+    ) -> pt.tensor:
         '''
         Метод для расчета максимальной просадки.
         Выбирается максимальное значение из вектора просадок.
         
-        :param alpha: Датафрейм с весами альфа-стратегии.
-        :param returns: Датафрейм с доходностью активов.
+        :param alpha: tensor с весами альфа-стратегии.
+        :param returns: tensor с доходностью активов.
         :return: Максимальная просадка.
         '''
         return FinCalculationsTensor.drawdown_vec(alpha, returns).max()
 
     @staticmethod
     def turnover_tensor(
-        alpha: pd.DataFrame
-    ) -> pd.Series:
+        alpha: pt.tensor
+    ) -> pt.tensor:
         '''
         Метод для расчета изменения (turnover) весов стратегии.
         Изменение веса вычисляется как разница между текущими весами альфы и весами на предыдущем шаге.
         
-        :param alpha: Датафрейм с весами альфа-стратегии.
-        :return: Серия изменений весов по каждому временному интервалу.
+        :param alpha: tensor с весами альфа-стратегии.
+        :return: tensor изменений весов по каждому временному интервалу.
         '''
-        return (alpha - alpha.shift()).abs().sum(axis=1)
+        alpha1=pd.DataFrame(alpha.numpy())
+        return pt.tensor((alpha1 - alpha1.shift()).abs().sum(axis=1))
     
     @staticmethod
     def turnover(
-        alpha: pd.DataFrame
+        alpha: pt.tensor
     ) -> float:
         '''
         Метод для расчета среднего изменения весов стратегии.
         
-        :param alpha: Датафрейм с весами альфа-стратегии.
+        :param alpha: tensor с весами альфа-стратегии.
         :return: Среднее изменение (turnover) весов стратегии.
         '''
         return FinCalculationsTensor.turnover_vec(alpha).mean()
     
     @staticmethod
     def decay(
-        alpha: pd.DataFrame,
+        alpha: pt.tensor,
         win: int
-    ) -> pd.DataFrame:
+    ) -> pt.tensor:
         '''
         Метод для расчета decay с экспоненциальным сглаживанием.
         
-        :param alpha: Датафрейм с весами альфа-стратегии.
+        :param alpha: tensor с весами альфа-стратегии.
         :param win: Параметр окна для экспоненциального сглаживания.
         :return: Альфа после сглаживания.
         '''
-
-        return alpha.ewm(span=win).mean() 
+        alpha1=pd.DataFrame(alpha.numpy())
+        return pt.tensor(alpha1.ewm(span=win).mean())
     
     @staticmethod
     def profit_margin(
-        alpha: pd.DataFrame,
-        returns: pd.DataFrame
-    ) -> float:
+        alpha: pt.tensor,
+        returns: pt.tensor
+    ) -> pt.tensor:
         '''
         Метод для расчета метрики Profit Margin.
 
-        :param alpha: Датафрейм с весами альфа-стратегии.
-        :param returns: Датафрейм с доходностью активов.
+        :param alpha: tensor с весами альфа-стратегии.
+        :param returns: tensor с доходностью активов.
         :return: Значение Profit Margin.
         '''
         pnl = FinCalculations.pnl_vec(alpha, returns)
@@ -146,9 +147,9 @@ class FinCalculationsTensor:
 
     @staticmethod
     def metrics(
-        alpha: pd.DataFrame,
-        returns: pd.DataFrame
-    ) -> pd.DataFrame:
+        alpha: pt.tensor,
+        returns: pt.tensor
+    ) -> pt.tensor:
         '''
         Метод для расчета всех основных метрик альфа-стратегии:
         - Доходность (PnL)
@@ -156,9 +157,9 @@ class FinCalculationsTensor:
         - Максимальная просадка
         - Оборот (turnover)
         
-        :param alpha: Датафрейм с весами альфа-стратегии.
-        :param returns: Датафрейм с доходностью активов.
-        :return: Датафрейм с рассчитанными метриками.
+        :param alpha: tensor с весами альфа-стратегии.
+        :param returns: tensor с доходностью активов.
+        :return: tensor с рассчитанными метриками.
         '''
         metrics_dict = {}
         metrics_dict["Pnl"] = FinCalculations.pnl(alpha, returns)
