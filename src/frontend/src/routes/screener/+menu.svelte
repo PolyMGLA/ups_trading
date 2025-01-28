98c54b352cf0c7f06d5b5a159bce8d5a06e64680
1VictorS1 <svalovviktor202@gmail.com>
2025-01-28 12:19:46 +0500

    Fixed: fixed color for screener

--- src/frontend/src/routes/screener/+menu.svelte
+++ src/frontend/src/routes/screener/+menu.svelte
@@ -7,7 +7,7 @@
   <style>
     body {
       font-family: Arial, sans-serif;
-      background-color: #121212;
+      background-color: #30313A;
       color: #FFFFFF;
       margin: 0;
       padding: 0;
@@ -18,14 +18,14 @@
       justify-content: space-between;
       align-items: center;
       padding: 20px;
-      background-color: #1F1F1F;
+      background-color: #30313A;
     }
   
     .back-button {
       padding: 10px 20px;
       border: none;
       background-color: #b1f5ec;
-      color: #121212;
+      color: rgb(255, 255, 255);
       border-radius: 8px;
       font-size: 16px;
       cursor: pointer;
@@ -40,11 +40,12 @@
     }
   
     .strategy-card {
-      background-color: #1F1F1F;
+      background-color: #30313A;
       padding: 20px;
       border-radius: 10px;
       box-shadow: 0 4px 10px rgba(0, 0, 0, 0.5);
       border: 1px solid #b1f5ec;
+      color: white;
     }
   
     .strategy-card h3 {
@@ -55,8 +56,9 @@
     .footer {
       text-align: center;
       padding: 20px;
-      background-color: #1F1F1F;
+      background-color: #30313A;
       border-top: 1px solid #b1f5ec;
+      color: white;
     }
   </style>
   
